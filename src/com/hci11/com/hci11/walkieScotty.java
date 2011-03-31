a6cb9b0a6115424178d853777024cb24ec7d73b9
MasterPie <vrp@.(none)>
2011-03-31 00:56:55 -0400

    Set up android project

---
+++ src/com/hci11/com/hci11/walkieScotty.java
@@ -0,0 +1,15 @@
+package com.hci11.com.hci11;
+
+import com.hci11.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class walkieScotty extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
