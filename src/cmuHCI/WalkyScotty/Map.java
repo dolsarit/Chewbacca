1dce1a4637315853ac47da9bc4ae0d0d65f265c2
Ben Cohen <bj.cohen19@gmail.com>
2011-03-31 21:36:17 -0400

    Added stuff for gMaps view.

--- src/cmuHCI/WalkyScotty/Map.java
+++ src/cmuHCI/WalkyScotty/Map.java
@@ -1,13 +1,24 @@
 package cmuHCI.WalkyScotty;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class Map extends Activity {
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class Map extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.map);
+        
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
